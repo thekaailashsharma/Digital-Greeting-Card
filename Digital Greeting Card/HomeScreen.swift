992e4913fd05bf6a01ac04d5b2537bab8f92ab3b
Kailash Sharma <kailashps.1011@gmail.com>
2024-03-08 20:41:09 +0530

    Add list of animations

---
+++ Digital Greeting Card/HomeScreen.swift
@@ -0,0 +1,34 @@
+//
+//  HomeScreen.swift
+//  Digital Greeting Card
+//
+//  Created by Kailash on 08/03/24.
+//
+
+import SwiftUI
+
+struct HomeScreen: View {
+    var body: some View {
+        NavigationStack {
+            ZStack {
+                Color("bg")
+                    .ignoresSafeArea()
+                
+                ScrollView {
+                    NavigationLink {
+                        
+                    } label: {
+                        
+                    }
+
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    NavigationStack {
+        HomeScreen()
+    }
+}
