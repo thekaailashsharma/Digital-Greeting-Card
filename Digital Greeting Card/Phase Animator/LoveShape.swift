cf5352a4857fa0b87489978b59312e1cd1a3a910
Kailash Sharma <kailashps.1011@gmail.com>
2024-03-09 23:43:00 +0530

    Add phase animations

---
+++ Digital Greeting Card/Phase Animator/LoveShape.swift
@@ -0,0 +1,18 @@
+//
+//  LoveShape.swift
+//  Digital Greeting Card
+//
+//  Created by Kailash on 09/03/24.
+//
+
+import SwiftUI
+
+struct LoveShape: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoveShape()
+}
