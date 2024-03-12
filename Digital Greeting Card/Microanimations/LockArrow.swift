f1b894b438129d379e3dc50473fee272642b7ab8
Kailash Sharma <kailashps.1011@gmail.com>
2024-03-13 02:21:28 +0530

    Shake Gesture

---
+++ Digital Greeting Card/Microanimations/LockArrow.swift
@@ -0,0 +1,18 @@
+//
+//  LockArrow.swift
+//  Digital Greeting Card
+//
+//  Created by Kailash on 11/03/24.
+//
+
+import SwiftUI
+
+struct LockArrow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LockArrow()
+}
---
+++ Digital Greeting Card/Microanimations/WifiView.swift
@@ -0,0 +1,18 @@
+//
+//  WifiView.swift
+//  Digital Greeting Card
+//
+//  Created by Kailash on 10/03/24.
+//
+
+import SwiftUI
+
+struct WifiView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WifiView()
+}
